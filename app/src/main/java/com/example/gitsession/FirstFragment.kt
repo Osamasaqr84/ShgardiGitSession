f7c356162e0e655e452175f4db4d22174e4d1755
Osamasaqr84 <osama@shgardi.app>
2022-11-29 13:51:16 +0200

    -add space  to fragment.

--- app/src/main/java/com/example/gitsession/FirstFragment.kt
+++ app/src/main/java/com/example/gitsession/FirstFragment.kt
@@ -13,6 +13,9 @@ import com.example.gitsession.databinding.FragmentFirstBinding
  */
 class FirstFragment : Fragment() {
 
+
+
+
     private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
